9b420d072cfc2f30923e77bda19fc412914fd8d0
vlad-shepelenko <vlshepelenk00@gmail.com>
2023-01-19 08:59:27 +0300

    Create adaptive

--- src/components/NotFound/NotFoundComponent.jsx
+++ src/components/NotFound/NotFoundComponent.jsx
@@ -1,22 +1,30 @@
 import "./notfound.scss";
+import { useNavigate } from "react-router-dom";
 
 const NotFoundComponent = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-    <section className = "notfound_container">
-    <section className="notfound_section">
-        <div className="notfound_text_container">
-          <h1 className="notfound_title">404</h1>
-          <h2 className="notfound_title_description">Страница не найдена</h2>
-          <span className="notfound_text">
-            Неправильно набран адрес или такая страница больше не существует
-          </span>
-        </div>
-        <div className="notfound_button_container">
-          <button className="notfound_button">Перейти на главную</button>
-        </div>
+      <section className="notfound_container">
+        <section className="notfound_section">
+          <div className="notfound_text_container">
+            <h1 className="notfound_title">404</h1>
+            <h2 className="notfound_title_description">Страница не найдена</h2>
+            <span className="notfound_text">
+              Неправильно набран адрес или такая страница больше не существует
+            </span>
+          </div>
+          <div className="notfound_button_container">
+            <button
+              onClick={() => navigate("/home")}
+              className="notfound_button"
+            >
+              Перейти на главную
+            </button>
+          </div>
+        </section>
       </section>
-    </section>
     </>
   );
 };
--- src/components/Thanks/ThanksComponent.jsx
+++ src/components/Thanks/ThanksComponent.jsx
@@ -1,23 +1,27 @@
 import { checkThanks } from "../../assets/images";
+import { useNavigate } from "react-router-dom";
 import "./thanks.scss";
 
 const ThanksComponent = () => {
+  const navigate = useNavigate();
   return (
     <>
-    <section className="thanks_container">
-    <section className="thanks_section">
-        <div className="thanks_text_container">
-          <img className="thanks_image" src={checkThanks} alt="thanks" />
-          <h1 className="thanks_title">Спасибо!</h1>
-          <span className="thanks_text">
-            Ваша заявка принята. Мы свяжемся с вами <br /> в ближайшее время
-          </span>
-        </div>
-        <div className="thanks_button_container">
-          <button className="thanks_button">Перейти на главную</button>
-        </div>
+      <section className="thanks_container">
+        <section className="thanks_section">
+          <div className="thanks_text_container">
+            <img className="thanks_image" src={checkThanks} alt="thanks" />
+            <h1 className="thanks_title">Спасибо!</h1>
+            <span className="thanks_text">
+              Ваша заявка принята. Мы свяжемся с вами <br /> в ближайшее время
+            </span>
+          </div>
+          <div className="thanks_button_container">
+            <button onClick={() => navigate("/home")} className="thanks_button">
+              Перейти на главную
+            </button>
+          </div>
+        </section>
       </section>
-    </section>
     </>
   );
 };
